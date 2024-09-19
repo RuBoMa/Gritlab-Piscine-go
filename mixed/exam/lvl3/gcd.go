bf227810f35ef3b7325cd3fbf90a7910c987b5ba
Roope Hongisto <roope@Roopes-Air.localdomain>
2024-09-19 12:55:06 +0300

    added gcd.go

--- mixed/exam/lvl3/gcd.go
+++ mixed/exam/lvl3/gcd.go
@@ -1,12 +1,19 @@
 package piscine
 
+// func Gcd(a, b uint) uint {
+// 	for a != b {
+// 		if a > b {
+// 			a -= b
+// 		} else {
+// 			b -= a
+// 		}
+// 	}
+// 	return a
+// }
+
 func Gcd(a, b uint) uint {
-	for a != b {
-		if a > b {
-			a -= b
-		} else {
-			b -= a
-		}
+	if b == 0 {
+		return a
 	}
-	return a
+	return Gcd(b, a%b)
 }
